f1d4051e2515b4e3b53f0db22c4d8843f8eb8569
tomorrow9913 <tomorrow9913.gmail.com>
2019-09-11 13:49:22 +0900

    보여주는 형식 수정

--- 190909/dice.cpp
+++ 190909/dice.cpp
@@ -2,23 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
-const int DiceMax = 100000;
-const int dice = 6;
+const int throwDiceMax = 100000;
+const int diceMax = 6;
+const int diceMin = 1;
 
 int main() {
     srand(time(NULL));
 
-    int arr[dice+ 1] = {0};
-
-    for (int i = 0; i < DiceMax; i++) {
-        int random = rand() % dice + 1;
-        arr[random]++;
+    int arrDiceCnt[diceMax+ 1] = {0};
+    
+    for (int i = 0; i < throwDiceMax; i++) {
+        int random;
+        
+        random = rand() % diceMax + diceMin;
+        arrDiceCnt[random]++;
 
         printf("[%06d] 주사위 눈은 %d\n", i+1, random);
     }
+    
+    double percentageNum[diceMax + 1] = {0};
+    for (int i = diceMin; i <= diceMax; i++) {
+        percentageNum[i] = ((double)arrDiceCnt[i] / throwDiceMax) * 100;
+    }
 
-    for(int i = 1; i <= dice; i++) {
-        printf("%d는 %d번 나왔습니다.\n", i, arr[i]);
+    for(int i = 1; i <= diceMax; i++) {
+        printf("%d 은 %d번 나왔습니다. (%.2lf %%)\n", i, arrDiceCnt[i], percentageNum[i]);
     }
 
     return 0;
